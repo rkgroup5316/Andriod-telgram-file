e621907a873c99435212c5163971071cac46876e
rkgroup5316 <rkgroup.coder@gmail.com>
2025-02-20 15:50:47 +0530

    Update build.gradle.kts

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 
 dependencies {
     implementation("com.android.tools.build:gradle:8.2.0")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
 }
 
 java {
