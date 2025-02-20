1ef8f1174014c03da2ca698de0193d9d069253c7
rkgroup5316 <rkgroup.coder@gmail.com>
2025-02-20 16:24:22 +0530

    Update build.gradle.kts

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,19 +1,2 @@
-plugins {
-    `kotlin-dsl`
-}
-
-repositories {
-    google()
-    mavenCentral()
-}
-
-dependencies {
-    implementation("com.android.tools.build:gradle:8.2.0")  // Use direct version temporarily
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")  // Use direct version temporarily
-}
-
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-}
+rootProject.name = "buildSrc"
+// No dependencyResolutionManagement block needed
