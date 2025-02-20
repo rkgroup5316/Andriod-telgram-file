65ab47d481792dd19b178afdbfc56c7bbfe86566
rkgroup5316 <rkgroup.coder@gmail.com>
2025-02-21 00:08:30 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,11 +15,7 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
     }
-    versionCatalogs {
-        create("libs") {
-            from(files("gradle/libs.versions.toml"))
-        }
-    }
+    
 }
 
 
