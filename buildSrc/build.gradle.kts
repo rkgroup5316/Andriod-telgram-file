d90f03e093c4f1effa15da5a12e9012090e232e9
rkgroup5316 <rkgroup.coder@gmail.com>
2025-02-20 16:33:40 +0530

    Update build.gradle.kts

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,2 +1,13 @@
-rootProject.name = "buildSrc"
-// No dependencyResolutionManagement block needed
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    google()
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.android.tools.build:gradle:8.2.0")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+}
