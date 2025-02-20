3d60c058bea247d1ceed2eaaadb66be4f0fcad49
rkgroup5316 <rkgroup.coder@gmail.com>
2025-02-20 09:56:55 +0530

    Update build.gradle.kts

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,14 +1,16 @@
 plugins {
     `kotlin-dsl`
 }
+
 repositories {
     google()
     mavenCentral()
 }
 
 dependencies {
-    implementation(libs.kgp)
-    implementation(libs.agp)
+    // Direct dependencies instead of version catalog
+    implementation("com.android.tools.build:gradle:8.2.0")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
