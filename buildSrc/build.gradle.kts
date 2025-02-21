c74070f0cb01e74a8106c559bb310aeaa2e09870
rkgroup5316 <rkgroup.coder@gmail.com>
2025-02-21 13:48:54 +0530

    Update build.gradle.kts

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,8 +7,7 @@ repositories {
     mavenCentral()
 }
 
-// Remove direct dependency declarations and use compileOnly
 dependencies {
-    compileOnly("com.android.tools.build:gradle:8.2.0")
-    compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+    implementation("com.android.tools.build:gradle:8.2.0")
 }
