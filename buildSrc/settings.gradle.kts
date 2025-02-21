26475a8fa3f7fc08841c23341de243ba63bf3d4f
rkgroup5316 <rkgroup.coder@gmail.com>
2025-02-21 13:59:35 +0530

    Update settings.gradle.kts

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -1,2 +1,8 @@
-rootProject.name = "buildSrc"
-
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+}
+rootProject.name = ("buildSrc")
