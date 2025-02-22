1a11d25acf61b6f9889348ba35063cd0d7edb592
rkgroup5316 <rkgroup.coder@gmail.com>
2025-02-22 09:24:19 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.kotlin.android) apply false
+    id("com.android.application") apply false
+    id("com.android.library") apply false
+    kotlin("android") apply false
     alias(libs.plugins.kotlin.kapt) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.detekt)
@@ -21,18 +21,10 @@ subprojects {
     }
 }
 
-tasks.withType<DependencyUpdatesTask> {
-    rejectVersionIf {
-        val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { 
-            candidate.version.uppercase().contains(it) 
+tasks {
+    withType<DependencyUpdatesTask>().configureEach {
+        rejectVersionIf {
+            candidate.version.isStableVersion().not()
         }
-        val regex = "^[0-9,.v-]+(-r)?$".toRegex()
-        val isStable = stableKeyword || regex.matches(candidate.version)
-        !isStable
     }
-    gradleReleaseChannel = "current"
-}
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
 }
