b680cbff9fa0cf03b3a5ca05308e4f0941dc1ab4
rkgroup5316 <rkgroup.coder@gmail.com>
2025-02-22 09:42:50 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,12 @@ plugins {
     id("com.android.application") apply false
     id("com.android.library") apply false
     kotlin("android") apply false
-    alias(libs.plugins.kotlin.kapt) apply false
-    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.detekt)
+    alias(libs.plugins.hilt) apply false
     alias(libs.plugins.versions)
+    cleanup
+    base
 }
 
 
